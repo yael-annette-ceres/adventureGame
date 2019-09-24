541f781485ac1f3f0cec74c656cbdf4f98005d40
Yael Brown <yaelrbrown@gmail.com>
2019-09-23 22:24:40 -0500

    Finished game

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -57,10 +57,12 @@ public class Main {
         System.out.println("===================="); // 20 ='s
         System.out.println("Round " + roundCount + "");
         System.out.println("====================\n"); // 20 ='s
+        robotThinking(3000);
         humanChoiceLogic(humanMoveChoice());
-        System.out.println("\nRobot's move!\n");
         robotChoiceLogic(robotMoveChoice());
+        robotThinking(2000);
         displayStats();
+        roundCount++;
     };
 
     // Validate input
@@ -79,7 +81,6 @@ public class Main {
                 System.out.println("Input is invalid. Please enter a letter:  ");
                 choice = sc.nextLine();
                 validateInput();
-
         }
         return choice;
     }
@@ -92,22 +93,30 @@ public class Main {
         choice = "";
         Character enteredChoice;
 
-        System.out.println(humanName + "'s Move !");
+        System.out.println("\n" + humanName + "'s Move !");
+        robotThinking(1000);
         System.out.println("A. Throw a beer at Robot");
         System.out.println("B. Hit robot with guitar");
         System.out.println("C. Drink a beer");
         System.out.println("D. Play a sick rift the crowd loves!");
 
         // Take in user input
-        System.out.println("Enter a letter: ");
+        System.out.println("\nEnter a letter: ");
         choice = sc.nextLine();
 
         // convert entered choice to string and lowercase
         choice = choice.toLowerCase();
 
-        System.out.println(choice);
-        return validateInput();
+        if (choice == "c" && HumanPotions <= 0) {
+            robotThinking(500);
+            System.out.println("You are out of beers, make another selection !");
+            robotThinking(500);
+            humanChoiceLogic(humanMoveChoice());
+        }
 
+        System.out.println("");
+        robotThinking(1000);
+        return validateInput();
     };
 
     // Human Choice Logic
@@ -129,13 +138,13 @@ public class Main {
                 System.out.println("You increased your health to " + HumanHP + ". You have " + HumanPotions + " remaining.");
                 break;
             case "d":
-                System.out.println(humanName + " played a sick riff the crowd loves!");
+                System.out.println(humanName + " played a sick riff the crowd loves it!");
                 RobotDef = RobotDef - defDecrementor;
+                robotThinking(1000);
                 System.out.println("You reduced the robot's defenses to " + RobotDef);
                 break;
             default:
                 System.out.println(":)");
-
         }
         System.out.println("");
     }
@@ -151,13 +160,18 @@ public class Main {
         Character enteredChoice;
 
         System.out.println("Robot's Move !");
+        robotThinking(3000);
         System.out.println("A. Throw a ipod");
         System.out.println("B. Taze the human");
         System.out.println("C. Do the robot shuffle");
-        System.out.println("D. Detune human's guitar!");
+        System.out.println("D. Detune human's guitar!\n");
+        robotThinking(1000);
 
+        // Print picture of robot
+        asciiRobot();
+        robotThinking(1000);
         // Take in user input
-        System.out.println("Robot is thinking!");
+        System.out.println("Robot is thinking!\n");
         robotThinking(5000);
 
         // Calculate robot choice
@@ -187,6 +201,15 @@ public class Main {
     // Human Choice Logic
     public static void robotChoiceLogic(String input){
 
+        // Some AI logic
+        if (RobotHP <= 50 && RobotPotions > 0) {
+            input = "c";
+        };
+
+        if (input == "c" && RobotPotions <= 0) {
+            input = "b";
+        }
+
         switch (input){
             case "a" :
                 System.out.println("Robot threw a ipod at human!");
@@ -200,10 +223,11 @@ public class Main {
                 System.out.println("Robot did a robot shuffle!");
                 RobotHP = RobotHP + potion;
                 RobotPotions--;
-                System.out.println("Robot increased it's health to " + RobotHP + ". Robot now has " + RobotPotions + " remaining.");
+                System.out.println("Robot increased it's health to " + RobotHP + ". Robot now has " + RobotPotions + " shuffles remaining.");
                 break;
             case "d" :
                 System.out.println("Robot detuned the guitar!");
+                robotThinking(1000);
                 System.out.println("The crowd is hating your solo!");
                 HumanDef = HumanDef - defDecrementor;
                 System.out.println("You reduced the human's defenses to " + HumanDef);
@@ -218,7 +242,8 @@ public class Main {
 
     // Display status
     public static void displayStats() {
-        System.out.println("Fight Stats ! ==================\n");
+        System.out.println("\n================================");
+        System.out.println("Fight Stats!\n");
         System.out.println("Human: ");
         System.out.println("HP: " + HumanHP);
         System.out.println("DEF: " + HumanDef);
@@ -227,7 +252,8 @@ public class Main {
         System.out.println("HP: " + RobotHP);
         System.out.println("DEF: " + RobotDef);
         System.out.println("Full batteries to shuffle: " + RobotPotions);
-        System.out.println("================================");
+        System.out.println("================================\n");
+        robotThinking(1000);
     }
 
 
@@ -240,36 +266,146 @@ public class Main {
         };
     };
 
+    // ASCII ART
+    // Robot ASCII art
+    public static void asciiRobot() {
+        System.out.println("       _______");
+        System.out.println("     _/       \\_");
+        System.out.println("    / |       | \\");
+        System.out.println("   /  |__   __|  \\");
+        System.out.println("  |__/((o| |o))\\__|");
+        System.out.println("  |      | |      |");
+        System.out.println("  |\\     |_|     /|");
+        System.out.println("  | \\           / |");
+        System.out.println("   \\| /  ___  \\ |/");
+        System.out.println("    \\ | / _ \\ | /");
+        System.out.println("     \\_________/");
+        System.out.println("      _|_____|_");
+        System.out.println(" ____|_________|____");
+        System.out.println("/                   \\ ");
+    }
 
+    public static void asciiSmallGuy() {
+        System.out.println("(ง'̀-'́)ง");
+    }
+
+    public static void asciiPrepareToFight() {
+        System.out.println("__________                                            __           ___________.__       .__     __  ");
+        System.out.println("\\______   \\______  ____ ___________ _______  ____   _/  |_  ____   \\_   _____/|__| ____ |  |___/  |_");
+        System.out.println(" |     ___|_  __ \\/ __ \\\\____ \\__  \\\\_  __ \\/ __ \\  \\   __\\/  _ \\   |    __)  |  |/ ___\\|  |  \\   __");
+        System.out.println(" |    |    |  | \\|  ___/|  |_> > __ \\|  | \\|  ___/   |  | (  <_> )  |     \\   |  / /_/  >   Y  \\  |  ");
+        System.out.println(" |____|    |__|   \\___  >   __(____  /__|   \\___  >  |__|  \\____/   \\___  /   |__\\___  /|___|  /__|  ");
+        System.out.println("                      \\/|__|       \\/           \\/                      \\/      /_____/      \\/      ");
+    }
+
+    public static void asciiHuzzah() {
+        robotThinking(1000);
+        System.out.println("Walking down Navarro Street, on the way to Codeup....");
+        robotThinking(3000);
+        System.out.println("\\( ﾟヮﾟ)/");
+        robotThinking(500);
+        System.out.println("-( ﾟヮﾟ)-");
+        robotThinking(500);
+        System.out.println("\\( ﾟヮﾟ)/");
+        robotThinking(500);
+        System.out.println("-( ﾟヮﾟ)-");
+        robotThinking(500);
+        System.out.println("\\( ﾟヮﾟ)/");
+        robotThinking(500);
+        System.out.println("-( ﾟヮﾟ)-");
+        robotThinking(500);
+        System.out.println("\\( ﾟヮﾟ)/");
+        robotThinking(500);
+        System.out.println("-( ﾟヮﾟ)-");
+        robotThinking(500);
+        System.out.println("\\( ﾟヮﾟ)/");
+        robotThinking(500);
+        System.out.println("-( ﾟヮﾟ)-");
+        robotThinking(500);
+        System.out.println("\\( ﾟヮﾟ)/");
+        robotThinking(500);
+        System.out.println("-( ﾟヮﾟ)-");
+        robotThinking(500);
+    }
+
+    public static void asciiHumanWords() {
+        System.out.println(".__                                 ");
+        System.out.println("|  |__  __ __  _____ _____    ____ ");
+        System.out.println("|  |  \\|  |  \\/     \\\\__  \\  /    \\ ");
+        System.out.println("|   Y  \\  |  /  Y Y  \\/ __ \\|   |  \\");
+        System.out.println("|___|  /____/|__|_|  (____  /___|  /");
+        System.out.println("     \\/            \\/     \\/     \\/");
+    }
+
+    public static void asciiVerse() {
+        System.out.println("____   _____________");
+        System.out.println("\\   \\ /   /   _____/");
+        System.out.println(" \\   Y   /\\_____  \\ ");
+        System.out.println("  \\     / /        \\");
+        System.out.println("   \\___/ /_______  /");
+        System.out.println("                 \\/ ");
+    }
+
+    public static void asciiRobotWords() {
+        System.out.println("__________      ___.           __  ");
+        System.out.println("\\______   \\ ____\\_ |__   _____/  |_ ");
+        System.out.println(" |       _//  _ \\| __ \\ /  _ \\   __\\");
+        System.out.println(" |    |   (  <_> ) \\_\\ (  <_> )  |  ");
+        System.out.println(" |____|_  /\\____/|___  /\\____/|__|  ");
+        System.out.println("        \\/           \\/            ");
+    }
+
+    public static void asciiExclamation() {
+        System.out.println("._.");
+        System.out.println("| |");
+        System.out.println("| |");
+        System.out.println(" \\|");
+        System.out.println(" __");
+        System.out.println(" \\/");
+    }
 
 
     public static void main(String[] args) {
-        // Testing
-//        System.out.println(RobotHP);
-//
-//        int attackTemp = wAttack();
-//
-//        RobotHP = RobotHP - (attackTemp - (RobotDef / 10));
-//
-//        System.out.println("attack was " + attackTemp);
-//        System.out.println(RobotHP);
-//
-//
-//        // lower defense and do same attack again
-//        RobotDef = RobotDef - 18;
-//        System.out.println("Robot defense is " + RobotDef);
-//
-//        RobotHP = 100;
-//        RobotHP = RobotHP - (attackTemp - (RobotDef / 10));
-//        System.out.println("Robot HP after same attack with lowered defense: " + RobotHP);
 
+        asciiHuzzah();
+        robotThinking(2000);
+        System.out.println("!");
+        robotThinking(4000);
+        System.out.println("... A wild robot appears.");
+        robotThinking(1000);
+        asciiRobot();
+        robotThinking(1000);
+        asciiHumanWords();
+        robotThinking(1000);
+        asciiVerse();
+        robotThinking(1000);
+        asciiRobotWords();
+        robotThinking(2000);
+        System.out.println("Who will defend humans from robots today? (Enter your name)");
+        humanName = sc.nextLine();
+        robotThinking(1000);
+        System.out.println("Thank you, " + humanName + " you are so brave!");
+        robotThinking(1000);
+        asciiSmallGuy();
+        robotThinking(1000);
+        asciiPrepareToFight();
+        robotThinking(2000);
 
-//        RobotHP = RobotHP + potion;
-//
-//        System.out.println(RobotHP);
+        while (HumanHP >= 0 && RobotHP >= 0) {
+            round();
+        }
 
-        round();
+        if (HumanHP <= 0) {
+            System.out.println("\nHumans have been defeated");
+            robotThinking(1000);
+            asciiRobot();
+        }
 
+        if (RobotHP <= 0) {
+            System.out.println("\nRobots have been defeated!");
+            robotThinking(1000);
+            asciiHuzzah();
+        }
     }
 
 
